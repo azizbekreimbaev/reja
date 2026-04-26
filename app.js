c1b735f0895d65907ce17fc4cf3c421a3d909ad8
Azizbek <azizbekreimbaev1003@gmail.com>
2026-04-26 20:21:02 +0900

    feat: mongodb running successfully

---
+++ app.js
@@ -0,0 +1,72 @@
+console.log("Web server creation stage");
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const fs = require('fs');
+
+
+//1. MongoDB chaqirish
+const db = require('./server').db();
+
+// console.log(db)
+
+let user;
+
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR", err);
+    } else {
+        user = JSON.parse(data)
+    }
+})
+
+
+// 1 Starting Code
+
+app.use(express.static("public"));  // static images css va boshqa elementlarni joylaymiz shu folderga
+app.use(express.json());   // Client va Server ortasiddagi data JSON formatda boladi shuni OBJECTga otqazib beradi
+app.use(express.urlencoded({ extended: true })); // html form post qilinganda input datalarni qanul qilib olish uchun kerak.
+
+
+// 2 Session code
+
+
+// 3 Views code
+//BSSR backendda frontend yasaymiz clientga yuiborish uchun.  EJS dan foydalanamiz views folder uchun
+
+app.set("views", "views");      //  views - views folderdan oladi
+app.set("view engine", "ejs"); // enngine ejs 
+
+
+// 4 Routing code
+// app.get("/", function(req, res) {
+//     res.end(`<p style = "font-size: 24px; font-weight: 900"> Hello World from MIT </p>`);
+// });
+
+// app.get("/about", function(req, res) {
+//     res.end(`<p style = "font-size: 24px; font-weight: 900"> Page About Us </p>`);
+// });
+
+// app.get("/payment", function(req, res) {
+//     res.end(`<p style = "font-size: 24px; font-weight: 900"> Payment Page </p>`);
+// });
+
+app.post("/create-item", (req, res) => {
+    console.log('====================================');
+    console.log(req.body);
+    // console.log(req);
+    res.json({ test: "success" })
+    console.log('====================================');
+},)
+
+app.get("/", (req, res) => {
+    res.render('reja');
+})
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+})
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,76 +1,27 @@
-console.log("Web server creation stage");
-
-const express = require('express');
-const app = express();
 const http = require('http');
-const fs = require('fs');
+const mongodb = require('mongodb');
+
+let db;
+// const connectionString = "mongodb+srv://aziz_reimbaev:wbag1DucHRN8UVo7@cluster0.bngytxk.mongodb.net/?retryWrites=true&w=majority"
+const connectionString = "mongodb+srv://aziz_reimbaev:wbag1DucHRN8UVo7@cluster0.bngytxk.mongodb.net/reja?retryWrites=true&w=majority";
 
 
-let user;
-
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if (err) {
-        console.log("ERROR", err);
-    } else {
-        user = JSON.parse(data)
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) console.log("ERROR on connection  MOngoDb");
+    else {
+        console.log("MongoDB connection succeed");
+        // console.log(client)
+        module.exports = client;
+        const app = require('./app');
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function () {
+            console.log(`The server is running successfully on port ===> ${PORT} , http://localhost:${PORT}`);
+        })
     }
-})
+});
 
-
-// 1 Starting Code
-
-app.use(express.static("public"));  // static images css va boshqa elementlarni joylaymiz shu folderga
-app.use(express.json());   // Client va Server ortasiddagi data JSON formatda boladi shuni OBJECTga otqazib beradi
-app.use(express.urlencoded({ extended: true })); // html form post qilinganda input datalarni qanul qilib olish uchun kerak.
-
-
-// 2 Session code
-
-
-// 3 Views code
-//BSSR backendda frontend yasaymiz clientga yuiborish uchun.  EJS dan foydalanamiz views folder uchun
-
-app.set("views", "views");      //  views - views folderdan oladi
-app.set("view engine", "ejs"); // enngine ejs 
-
-
-// 4 Routing code
-// app.get("/", function(req, res) {
-//     res.end(`<p style = "font-size: 24px; font-weight: 900"> Hello World from MIT </p>`);
-// });
-
-// app.get("/about", function(req, res) {
-//     res.end(`<p style = "font-size: 24px; font-weight: 900"> Page About Us </p>`);
-// });
-
-// app.get("/payment", function(req, res) {
-//     res.end(`<p style = "font-size: 24px; font-weight: 900"> Payment Page </p>`);
-// });
-
-app.post("/create-item", (req, res) => {
-    console.log('====================================');
-    console.log(req.body);
-    // console.log(req);
-    res.json({ test: "success" })
-    console.log('====================================');
-},)
-
-app.get("/", (req, res) => {
-    res.render('reja');
-})
-
-app.get("/author", (req, res) => {
-    res.render("author", { user: user });
-})
-
-
-const server = http.createServer(app);
-
-let PORT = 3000;
-
-
-server.listen(PORT, function () {
-    console.log(`The server is running successfully on port ===> ${PORT} , http://localhost:${PORT}`);
-
-})
 
